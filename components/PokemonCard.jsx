7514ad08188e78d9e3f386f631390b70194d67a3
gragra-kj <muthuigracia27@gmail.com>
2024-05-16 13:20:54 +0300

    firsst commit

---
+++ components/PokemonCard.jsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function PokemonCard(){
+  return (
+    <View>
+      <Text>PokemonCard</Text>
+    </View>
+  )
+}
+// const styles=StyleSheet.create({
+//     container:{
+//         flex:1,
+//         backgroundColor:'f5f5f5'
+//     }
+// })
